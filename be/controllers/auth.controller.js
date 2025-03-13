e49cdfa9b4aca6e8da971afa631b42e40481afb9
munendra <munendrashiv97@gmail.com>
2025-03-13 22:34:28 +0530

    added mongodb

--- be/controllers/auth.controller.js
+++ be/controllers/auth.controller.js
@@ -8,14 +8,3 @@ export const signup = (req, res) => {
         
 }
 
-export const login = (req, res) => {
-    res.status(200).json({
-        message:"Login success"
-    })
-};
-
-export const signout = (req, res) => {
-    res.status(200).json({
-        message:"Signout success"
-    })
-};
