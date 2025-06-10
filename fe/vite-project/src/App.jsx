805ead492468b989129d157dfdf82417c8728133
munendra <munendrashiv97@gmail.com>
2025-06-10 23:59:05 +0530

    added profile page and post page

--- fe/vite-project/src/App.jsx
+++ fe/vite-project/src/App.jsx
@@ -4,6 +4,8 @@ import LoginPage from "./pages/auth/login/LoginPage";
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto bg-black min-h-screen'>
@@ -12,6 +14,8 @@ function App() {
         <Route path='/' element={<HomePage/>} />
         <Route path='/signup' element={<SignUpPage/>} />
         <Route path='/login' element={<LoginPage/>} />
+        <Route path='/NotificationPage' element={<NotificationPage/>} />
+        <Route path='/profile/:username' element={<ProfilePage/>} />
       </Routes>
       <RightPanel />
     </div>
