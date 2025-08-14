3b6e969b7b5e59f66ef64bc46f3c65675a1ae2df
munendra <munendrashiv97@gmail.com>
2025-08-14 22:13:03 +0530

    correcting the fe structure

--- fe/vite-project/src/App.jsx
+++ fe/vite-project/src/App.jsx
@@ -1,41 +1,11 @@
-import HomePage from "./pages/Home/HomePage";
-import SignUpPage from "./pages/auth/signup/SignUpPage";
-import LoginPage from "./pages/auth/login/LoginPage";
-import { Routes, Route } from 'react-router-dom';
-import Sidebar from "./components/common/Sidebar";
-import RightPanel from "./components/common/RightPanel";
-import NotificationPage from "./pages/notification/NotificationPage";
-import ProfilePage from "./pages/profile/ProfilePage";
-import { Toaster } from 'react-hot-toast';
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAuthUser } from "./redux/authSlice";
-import { Navigate } from "react-router-dom";
-
+import React from 'react';
 function App() {
-  const dispatch = useDispatch();
-  const { user} = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    dispatch(fetchAuthUser());
-  }, [dispatch]);
-
-  console.log("Auth User Data:", user);
-
   return (
-    <div className='flex max-w-6xl mx-auto bg-black min-h-screen'>
-     {user ? <Sidebar/> :null}
-      <Routes>
-        <Route path='/' element={user ? <HomePage /> : <Navigate to="/login" replace />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/notifications' element={user ? <NotificationPage />: <Navigate to="/login" replace/>} />
-        <Route path='/profile/:username' element={user ?<ProfilePage /> :<Navigate to="/login" replace/>} />
-      </Routes>
-     {user ? <RightPanel /> : null}
-      <Toaster />
-    </div>
+	<div class="flex items-center justify-center ">
+  <p class="text-red-500 text-center">hello world</p>
+</div>
   );
 }
+
 
 export default App;
--- fe/vite-project/src/main.jsx
+++ fe/vite-project/src/main.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-    <App/>
-    </QueryClientProvider>
-    </BrowserRouter>
-  </Provider>
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+				<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
