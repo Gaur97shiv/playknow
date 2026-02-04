487df44e7b4c65ce6411d3732d38a801b90f5d74
munendra <munendrashiv97@gmail.com>
2026-02-04 23:20:00 +0530

    corrected the port in vite.config file

--- fe/vite-project/vite.config.js
+++ fe/vite-project/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-        plugins: [react()],
-        server: {
-                port: 5000,
-                host: "0.0.0.0",
-                allowedHosts: true,
-                proxy: {
-                        "/api": {
-                                target: "http://localhost:3001",
-                                changeOrigin: true,
-                        },
-                },
-        },
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
